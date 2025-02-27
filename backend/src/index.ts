c09d50205aa5627dd76abbbbf4f025998273ed1c
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-02-28 02:47:54 +0530

    Solved signup post problem

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,39 +1,54 @@
-import { PrismaClient } from '@prisma/client';
-import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { Hono } from 'hono';
 import { sign } from 'hono/jwt';
 
-interface Env{
+interface Env {
   DATABASE_URL: string;
   JWT_SECRET: string;
 }
 
-const app = new Hono<{Bindings:Env}>();
+const app = new Hono<{Bindings: Env}>();
 
 app.post('/api/v1/signup', async (c) => {
-
-  const prisma = new PrismaClient ({
-    datasources: {
-      db:{
-        url: c.env.DATABASE_URL,
+  try {
+    
+    //PrismaClient instance with the Accelerate URL from env
+    const prisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: c.env.DATABASE_URL,
+        },
       },
-    },
-  });
-
-  const body = await c.req.json();
-
-  const user = await prisma.user.create({
-    data: {
-      email: body.email,
-      password: body.password,
-    },
-  })
-
-  const token = sign({id: user.id, name: user.name, email: user.email}, c.env.JWT_SECRET);
-
-  return c.json({
-    jwt_token : token,
-  })
-})
+    }).$extends(withAccelerate());
+    
+    const body = await c.req.json();
+    
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name,
+      },
+    });
+    
+    const token = await sign(
+      { id: user.id, name: user.name, email: user.email }, 
+      c.env.JWT_SECRET
+    );
+    
+    return c.json({
+      jwt_token: token,
+    });
+  } catch (error) {
+    console.error("Error occurred:", error);
+    
+    // Error response
+    return c.json({
+      error: error instanceof Error ? error.message : 'Unknown error occurred'
+    }, 500);
+  }
+});
 
 app.post('/api/v1/signin',(c) => {
   return c.text('signin route')
