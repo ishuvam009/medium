8921c2697c9c3069d9f41f8b4b59dbe754081591
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-03-07 00:47:41 +0530

    Updated user.ts

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,86 +1,118 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { verify,sign } from "hono/jwt";
-import { userValidation } from "../zod/zod";
+import { sign } from "hono/jwt";
+import { userSignin, userSignup } from "@zedeye/zod-medium-common";
 
-interface Env{
-    DATABASE_URL: string,
-    JWT_SECRET: string,
+interface Env {
+  DATABASE_URL: string;
+  JWT_SECRET: string;
 }
 
-export const userRouter = new Hono<{Bindings: Env}>();
+export const userRouter = new Hono<{ Bindings: Env }>();
 
-
-userRouter.post('/signup', async (c) => {
-    try {
-      
-      //PrismaClient instance with the Accelerate URL from env
-      const prisma = new PrismaClient({
-        datasources: {
-          db: {
-            url: c.env.DATABASE_URL,
-          },
-        },
-      }).$extends(withAccelerate());
-      
-      const body = await c.req.json();
-
-      const { success } = userValidation.safeParse(body);
-
-      if(!success){
-        c.status(411);
-        return c.json({
-          message: "Inputs are not correct."
-        })
-      }
-      
-      //zod and hased password
-      const user = await prisma.user.create({
-        data: {
-          email: body.email,
-          password: body.password,
-          name: body.name,
-        },
-      });
-      
-      const token = await sign(
-        { id: user.id, name: user.name, email: user.email }, 
-        c.env.JWT_SECRET
-      );
-      
-      return c.json({
-        jwt_token: token,
-      });
-    } catch (error) {
-      console.error("Error occurred:", error);
-      
-      // Error response
-      return c.json({
-        error: error instanceof Error ? error.message : 'Unknown error occurred'
-      }, 500);
-    }
-  });
-  
-  
-  
-userRouter.post('/signin',async (c) => {
-  
-    const prisma =  new PrismaClient({
+userRouter.post("/signup", async (c) => {
+  try {
+    //PrismaClient instance with the Accelerate URL from env
+    const prisma = new PrismaClient({
       datasources: {
-        db:{
-          url: c.env?.DATABASE_URL,
-        }
-      }
+        db: {
+          url: c.env.DATABASE_URL,
+        },
+      },
     }).$extends(withAccelerate());
-  
+
     const body = await c.req.json();
+
+    const { success } = userSignup.safeParse(body);
+
+    if (!success) {
+      c.status(411);
+      return c.json({
+        message: "Inputs are not correct.",
+      });
+    }
+
+    //Hased password
+    const user = await prisma.user.create({
+      data: {
+        email: body.username,
+        password: body.password,
+        name: body.name,
+      },
+    });
+
+    const token = await sign(
+      { id: user.id, name: user.name, email: user.email },
+      c.env.JWT_SECRET
+    );
+
+    return c.json({
+      jwt_token: token,
+    });
+  } catch (error) {
+    console.error("Error occurred:", error);
+
+    // Error response
+    return c.json(
+      {
+        error:
+          error instanceof Error ? error.message : "Unknown error occurred",
+      },
+      500
+    );
+  }
+});
+
+userRouter.post("/signin", async (c) => {
+  try {
+    const prisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: c.env?.DATABASE_URL,
+        },
+      },
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    const { success } = userSignin.safeParse(body);
+
+    if (!success) {
+      c.status(411);
+      return c.json({
+        message: "Inputs are not correct",
+      });
+    }
+
     const user = await prisma.user.findUnique({
       where: {
         email: body.email,
         password: body.password,
-      }
-    })
-  
-    return c.text('signin route')
-  });  
+      },
+    });
+
+    if (!user) {
+      c.status(401);
+      return c.json({
+        message: "Either Uername or Password or both are incorrect",
+      });
+    }
+
+    const token = await sign(
+      { id: user.id, username: user.email, password: user.password },
+      c.env.JWT_SECRET
+    );
+
+    return c.json({
+      jwt_token: token,
+    });
+  } catch (error) {
+    console.error("Error Occured", error);
+    
+    c.status(500);
+
+    return c.json({
+      error: error instanceof Error ? error.message : "Unknown error occurred"
+    }) 
+  }
+});
