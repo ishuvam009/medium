77c6bb6025f4ba68c38c775c0c2816a25dd0f2de
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-03-08 01:30:13 +0530

    Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { motion } from "motion/react"
+
+function App() {
+
+  return (
+    <>
+      <div className=" flex items-center justify-center h-screen w-screen bg-gradient-to-r from-purple-800 to-purple-900">
+        <div className="flex items-center justify-center h-40 w-64">
+          <p className="text-6xl text-white font-medium">Hello</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
