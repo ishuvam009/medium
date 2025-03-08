37ac2b027aac2bdaa1bee4a4323fe6e24eec6743
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-03-08 23:07:34 +0530

    Added Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing } from "./pages/Landing";
+import { Blog } from "./pages/Blog";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 
 function App() {
-
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signup" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
