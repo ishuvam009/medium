63b343f5a4bd9658fe98bb4125822d386777ca8f
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-03-13 23:56:30 +0530

    Commit

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+
 export const Signup = () => {
   return (
     <>
       <div className="h-full flex">
-        <div className="w-1/2 h-screen bg-white">hi</div>
-        <div className="w-1/2 h-screen bg-slate-100">ok</div>
+        <div className="w-1/2 h-screen bg-white">
+          <p>Create an account</p>
+          <p>Already have an account? <Link to={"/signin"}></Link></p>
+        </div>
+        <div className="w-1/2 h-screen bg-slate-100 justify-center content-center px-28">
+          <p className="text-4xl font-medium">"The customer service I received was exceptional. The support went above and beyond to address my concerns."</p>
+          <p className="text-xl font-medium mt-8">Jules Winnfield</p>
+          <p className="text-slate-500">CEO, Acme Inc</p>
+        </div>
       </div>
     </>
   );
