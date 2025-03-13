e641a3bfc54a880f87f0ed6be4c10d0b239b61be
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-03-13 23:42:03 +0530

    Commit

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,5 +1,10 @@
 export const Signup = () => {
-  return <>
-    <p>Hello</p>
-  </>;
+  return (
+    <>
+      <div className="h-full flex">
+        <div className="w-1/2 h-screen bg-white">hi</div>
+        <div className="w-1/2 h-screen bg-slate-100">ok</div>
+      </div>
+    </>
+  );
 };
