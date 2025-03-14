880dfcdf8ef0c805b26919679f909c4746da44d7
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-03-15 01:47:45 +0530

    Commit

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,23 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { UserSinUp } from "@zedeye/zod-medium-common";
+import axios from "axios";
 
 export const Signup = () => {
+
+  const [formData, setFormData] = useState<UserSinUp>({
+    email:"",
+    password:"",
+    name:""
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    const {id, value} = e.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [id]: value
+    }))};
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      
+      try {
+        const response = await axios.post("",formData);
+        console.log("The data is: ", response.data);
+      } catch (error) {
+        console.log("Error: ",error);
+      }
+    }
+  
+
   return (
     <>
       <div className="h-full flex">
-        <div className="w-1/2 h-screen bg-white text-center md:mt-10 xl:mt-20 2xl:mt-30">
+        <div className="w-full lg:w-1/2 h-screen bg-white text-center md:mt-10 xl:mt-20 2xl:mt-30">
           <p className="text-4xl font-bold">Create an account</p>
           <p className="text-slate-500 mt-2 lg:mt-4 xl:mt-8">
             Already have an account? <Link className="ml-2 hover:text-slate-700 underline" to={"/signin"}>Login</Link>
           </p>
 
-          <div className="mt-10 flex flex-col gap-6 max-w-md mx-auto px-4">
+          <form onSubmit={handleSubmit} className="mt-10 flex flex-col gap-6 max-w-md mx-auto px-4">
             <div className="flex flex-col items-start">
               <label className="font-medium mb-2" htmlFor="username">Username</label>
               <input 
-                id="username"
+                id="name"
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" 
                 type="text" 
                 placeholder="Enter your name"
+                value={formData.name}
+                onChange={handleInputChange}
               />
             </div>
 
@@ -28,6 +59,8 @@ export const Signup = () => {
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" 
                 type="email" 
                 placeholder="Enter your email"
+                value={formData.email}
+                onChange={handleInputChange}
               />
             </div>
 
@@ -38,14 +71,14 @@ export const Signup = () => {
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500" 
                 type="password" 
                 placeholder="Enter your password"
+                value={formData.password}
+                onChange={handleInputChange}
               />
             </div>
             <div>
-              <Link to={"/"}>
-              <button className="text-white bg-black w-full p-2 rounded-xl">Signup</button>
-              </Link>
+              <button type="submit" className="text-white bg-black w-full p-2 rounded-xl">Signup</button>
             </div>
-          </div>
+          </form>
           
         </div>
 
