55a17a6197997c0da98e99faf13fccdfa606a65c
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-03-17 04:20:27 +0530

    Commit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ import { LiaEditSolid } from "react-icons/lia";
 export const Header = () => {
   return (
     <>
-      <div className="flex justify-between border border-[#F2F2F2] mb-6">
+      <div className="flex justify-between mb-6">
         <div className="flex p-4 items-center justify-between gap-x-4">
           <div className="">
             <p className="text-2xl font-semibold font-serif">Medium</p>
@@ -14,7 +14,7 @@ export const Header = () => {
           </div>
           <div>
             <input
-              className="bg-[#F9F9F9] rounded-2xl"
+              className="bg-[#F9F9F9] rounded-2xl p-2"
               type="text"
               placeholder="  Search"
             />
@@ -22,8 +22,10 @@ export const Header = () => {
         </div>
         <div className="flex justify-between items-center p-4 gap-x-4">
           <div className="flex justify-between gap-x-1 items-center ">
-            <LiaEditSolid />
-            Write
+            <a className="flex justify-between items-center" href="">
+              <LiaEditSolid />
+              Write
+            </a>
           </div>
           <div>Profile</div>
         </div>
--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -4,8 +4,13 @@ export const Blog = () => {
   return (
     <>
       <Header />
-      <div>
-        <p>Hi</p>
+      <div className="flex border-t border-[#F2F2F2] ">
+        <div className="w-full lg:w-2/3 p-4 mx-20">
+          <p>Hi</p>
+        </div>
+        <div className="hidden lg:block md:hidden lg:w-1/3 h-screen border-l border-[#F2F2F2] p-4">
+          <p>Hi</p>
+        </div>
       </div>
     </>
   );
