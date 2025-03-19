5b94a1901a6d340bf930bbca10817953ef314d89
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-03-19 23:26:40 +0530

    Commit

--- frontend/src/components/Article.tsx
+++ frontend/src/components/Article.tsx
@@ -24,18 +24,53 @@ export const Article = () => {
             S
           </div>
           <div className="">Shuvam</div>
+          <div className="text-slate-400">5 min read</div>
+          <div className="text-slate-400">2 days ago</div>
         </div>
         <div className="mb-6">
           <p className="text-slate-500">18 March 2025</p>
         </div>
-        <div>
+        <div className="flex flex-col gap-y-8">
           <p>
-            Artificial general intelligence is a type of highly autonomous
-            artificial intelligence that matches or surpasses human cognitive
-            capabilities across most or all economically valuable work or
-            cognitive labor. This contrasts with narrow AI, which is limited to
-            specific tasks.
+            Artificial general intelligence (AGI) is the representation of
+            generalized human cognitive abilities in software so that, faced
+            with an unfamiliar task, the AGI system could find a solution. The
+            intention of an AGI system is to perform any task that a human being
+            is capable of.
           </p>
+          <p>
+            Definitions of AGI vary because experts from different fields define
+            human intelligence from different perspectives. Computer scientists
+            often define human intelligence in terms of being able to achieve
+            goals. Psychologists, on the other hand, often define general
+            intelligence in terms of adaptability or survival.
+          </p>
+          <p>
+            AGI is considered to be strong artificial intelligence (AI). Strong
+            AI contrasts with weak or narrow AI, which is the application of
+            artificial intelligence to specific tasks or problems. IBM's Watson
+            supercomputer, expert systems and self-driving cars are examples of
+            narrow AI.
+          </p>
+          <p className="font-bold text-2xl">What can artificial general intelligence do?</p>
+          <p>
+            AGI in computer science is an intelligent system with comprehensive
+            or complete knowledge and cognitive computing capabilities. As of
+            publication, no true AGI systems exist; they remain the stuff of
+            science fiction. The theoretical performance of these systems would
+            be indistinguishable from that of a human. However, the broad
+            intellectual capacities of AGI would exceed human capacities because
+            of its ability to access and process huge data sets at incredible
+            speeds.
+          </p>
+          <p>
+            True AGI should be capable of executing human-level tasks and
+            abilities that no existing computer can achieve. Today, AI can
+            perform many tasks but not at the level of success that would
+            categorize them as human or general intelligence.
+          </p>
+          <p></p>
+          <p></p>
         </div>
       </div>
     </>
