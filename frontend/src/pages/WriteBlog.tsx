7ec00bc47b08bdd026d94be8c57553b9b308351a
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-03-22 23:36:10 +0530

    Commit

--- frontend/src/pages/WriteBlog.tsx
+++ frontend/src/pages/WriteBlog.tsx
@@ -5,8 +5,8 @@ export const WriteBlog = () => {
     <>
       <div>
         <Header />
-        <div>
-          <p>Write the Blog</p>
+        <div className="flex flex-col gap-y-4 item-center bg-amber-100">
+          <p className="text-2xl font-bold">Write the Blog</p>
           <input type="text" placeholder="Write here." />
         </div>
       </div>
